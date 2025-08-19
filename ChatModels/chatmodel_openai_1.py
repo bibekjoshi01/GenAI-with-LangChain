a0e6b7474029eb105938c39ec07b36af36ed8bee
Bibek Joshi <bibekjoshi34@gmail.com>
2025-08-19 16:08:13 +0545

    openai

--- ChatModels/chatmodel_openai_1.py
+++ ChatModels/chatmodel_openai_1.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-chat_model = ChatOpenAI(model='gpt-4', temperature=1)
+chat_model = ChatOpenAI(model="gpt-4", temperature=1, max_completion_tokens=1000)
 
 resut = chat_model.invoke("What is the capital city of Nepal?")
 
