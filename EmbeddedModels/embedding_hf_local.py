e9de792f0fe8a23bb96577490432a062eba5c270
Bibek Joshi <bibekjoshi34@gmail.com>
2025-08-24 20:13:53 +0545

    fix: embedding

--- EmbeddedModels/embedding_hf_local.py
+++ EmbeddedModels/embedding_hf_local.py
@@ -1,12 +1,13 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
+
 os.environ["HF_HOME"] = "/Users/bibekjoshi01/Desktop/Langchain/models"
 
-embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-text = [ "My name is bibek", "This is student"]
+text = ["My name is bibek", "This is student"]
 
 # vector = embedding.embed_query(text)
-vector = embedding  .embed_documents(texts=text)
+vector = embedding.embed_documents(texts=text)
 
 print(vector)
